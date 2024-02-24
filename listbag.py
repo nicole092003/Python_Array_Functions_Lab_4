fe98b186b0a90e05025699fe504e2c065bbfe229
Dorita Nicole Thomas <dnthomas@my.waketech.edu>
2024-02-23 23:36:09 -0500

    Lab 05 Bags

--- listbag.py
+++ listbag.py
@@ -1,43 +1,53 @@
+# Nicole Thomas
+# Lab05
+# February 23, 2024,
+
+
 """
 File: listbag.py
 
 A list-based implementation of bags
 """
 
+
 class ListBag(object):
 
     # Constructor
-    def __init__(self, sourceCollection = None):
+    def __init__(self, sourceCollection=None):
         """Sets the initial state of self, which includes the
         contents of sourceCollection, if it's present."""
-        pass
+        self.items = []
+        if sourceCollection:
+            self.items.extend(sourceCollection)
 
     # Accessor methods
     def isEmpty(self):
         """Returns True if the bag is empty, or False otherwise."""
-        pass
-    
+        return len(self.items) == 0
+
     def __len__(self):
         """Returns the number of items in self."""
-        pass
+        return len(self.items)
 
     def __str__(self):
         """Returns the string representation of self."""
-        pass
+        return "{" + ",".join(map(str, self.items)) + "}"
 
     def __iter__(self):
         """Supports iteration over a view of self."""
-        pass
+        return iter(self.items)
 
     def __add__(self, other):
         """Returns a new bag containing the contents
         of self and other."""
-        pass
+        new_bag = ListBag(self.items)
+        new_bag.items.extend(other.items)
+        return new_bag
 
     def __eq__(self, other):
         """Returns True if self equals other,
         or False otherwise."""
-        pass
+        return set(self.items) == set(other.items)
 
     def count(self, item):
         """Returns the number of instances of item in self."""
@@ -46,20 +56,22 @@ class ListBag(object):
     # Mutator methods
     def clear(self):
         """Makes self become empty."""
-        pass
+        self.items = []
 
     def add(self, item):
         """Adds item to self."""
-        pass
+        self.items.append(item)
 
     def remove(self, item):
         """Remove item from self.
         Raises KeyError if item is not in self."""
-        pass
+        try:
+            self.items.remove(item)
+        except ValueError:
+            raise KeyError(f"{item} not in the bag")
 
 
 def main():
-
     lyst = list(range(1, 11))
     print("The list of items added:", lyst)
     b = ListBag(lyst)
@@ -87,6 +99,7 @@ def main():
     c.clear()
     print("c.clear()")
     print("bag c's string:", c)
+
 
 if __name__ == "__main__":
     main()
